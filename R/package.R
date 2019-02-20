f0a9240a9d020baf388f7e51889ac770b55e9d00
young-geun <dudrmshub@gmail.com>
2019-02-20 21:55:17 +0900

    package script file

---
+++ R/package.R
@@ -0,0 +1,10 @@
+#' Private tools to document in R-markdown
+#'
+#' @description
+#' This is an assistant tool for R Markdown, mainly pdf
+#' @docType package
+#' @name rmdtool-package
+#' @aliases rmdtool
+#' @author Young-geun Kim <\url{https://github.com/ygeunkim}>
+#' @importFrom knitr knit_print asis_output
+NULL
