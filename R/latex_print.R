d6ebdc1c4b085357d535090302a3885d8c7d6a7a
young-geun <dudrmshub@gmail.com>
2019-02-20 20:45:46 +0900

    import knit_print in the package

--- R/latex_print.R
+++ R/latex_print.R
@@ -70,11 +70,13 @@ as_tex.matrix <- function(x, ...) {
 #' A <- matrix(1:10, nrow = 2)
 #' knitr_print(as_tex(A))
 #' ```
+#' @importFrom knitr knit_print
+
 #' @export
 #' @rdname knit_print
 knit_print.tex <- function(x, inline = FALSE, ...) {
   if (inline) {
-    print(x)
+    x
   } else {
     asis_output(
       paste0("$$", x, "$$")
