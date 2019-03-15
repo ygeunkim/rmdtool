8bae5a2f2ad595311b063942fd69c0b9c0d9f2d4
young-geun <dudrmshub@gmail.com>
2019-03-15 20:00:02 +0900

    mathjax equation numbering

---
+++ R/eqn_ref.R
@@ -0,0 +1,22 @@
+#' Equation reference in html documents
+#'
+#' @description
+#' By writing this script at the beginning of the rmd file, equation referencing becomes more flexible in html documents.
+#' @examples
+#' eqn_numbering()
+#' @return Copy and paste to the document
+#' @export
+eqn_numbering <- function() {
+  jax <- paste0(eqn_mathjax(), collapse = "\n")
+  writeLines(jax)
+}
+
+eqn_mathjax <- function() {
+  c(
+    "<script type=\"text/x-mathjax-config\">",
+    "    MathJax.Hub.Config({",
+    "      TeX: { equationNumbers: { autoNumber: \"AMS\" } }",
+    "    });",
+    "</script>"
+  )
+}
